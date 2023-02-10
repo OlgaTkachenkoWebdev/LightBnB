64c69b31371c20ad75bb6fe3220b0a1b4a8b74b3
woodenonesie <olga.pi4ugina@yandex.ru>
2023-02-09 22:07:23 -0800

    fix get reservations bug

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,7 +98,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
-    GROUP BY properties.id, reservations.id, property_reviews.id
+    GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
     LIMIT $2;`,
       [guest_id, limit])
